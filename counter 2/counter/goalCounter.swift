ef22789261de84fc13ba4d2648fb53836ad813a7
Dogwar266 <34766382+Dogwar266@users.noreply.github.com>
2019-05-07 20:07:51 +1000

    Add files via upload
    Added Reubens code for the Arm/Trigger buttons. 
    
    Currently not working correctly

--- counter 2/counter/goalCounter.swift
+++ counter 2/counter/goalCounter.swift
@@ -25,6 +25,59 @@ class goalCounter: UIViewController {
         print(timerLabel.text)
         
     }
+    var count:Int = 0 //value to be displayed on the counter
+    var armed:Bool = false //used for checking if the counter has been armed
+    /*
+     The arm function is used to arm the counter, making it ready to trigger.
+     */
+    func arm() -> Void
+    {
+        if armed == false
+        {
+            armed = true
+        }
+    }
+    /*
+     The trigger function is used to either increment the counter, if it has been armed, or to display a message telling the user they pressed the wrong button.
+     */
+    func trigger() -> Void
+    {
+        if armed == true
+        {
+            if count >= 0
+            {
+                count += 1 //increase the value of count
+                goalCount.text = String(count) //change the label that displays the counter value
+                armed = false //de-arms the counter so its can be armed again
+                print(count)
+            }
+        }
+        else
+        {
+            let alert = UIAlertController(title: "Error", message: "You need to hit the arm button before you can trigger the counter.", preferredStyle: .alert) //sets up the alert message for when he user presses the wrong button combonation
+            
+            alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil)) //adds a continue button to the alerts so the user isn't stuck in error message hell.
+            
+            self.present(alert, animated: true)//presents the alert
+        }
+        
+    }
+    func reset() -> Void
+    {
+        count = 0
+        goalCount.text = String(count)
+    }
+    @IBOutlet weak var countDisplay: UILabel! //declares our counter display in the code
+    @IBAction func arm(_ sender: Any) { //links our arm button and tells it to run the arm function when pressed
+        arm()
+    }
+    @IBAction func trigger(_ sender: Any) { //links our trigger button and tells it to run the trigger function when presed
+        trigger()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        reset()
+    }
     
 
     /*
