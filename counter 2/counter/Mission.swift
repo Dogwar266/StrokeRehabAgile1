5808f9bd4e066817f07c7e9e14b4915b28ad43b7
Edward Boreham <edward.boreham@gmail.com>
2019-05-15 11:37:48 +1000

    Merged the database files

---
+++ counter 2/counter/Mission.swift
@@ -0,0 +1,28 @@
+
+//
+//  Mission.swift
+//  StrokeRehabilitation
+//
+//  Created by yinzixie on 4/4/19.
+//  Copyright © 2019 Project. All rights reserved.
+//
+import Foundation
+
+class Mission {
+    
+    var MissionID:Int
+    var MissionType:CounterModel
+    var AimNumber:Int?
+    var AimTime:Int?
+    var FinalNumber:Int = 0
+    var FinalTime:Int? = nil
+
+    
+    init(id:Int,type:CounterModel,number:Int?,time:Int?){
+        MissionID = id
+        MissionType = type
+        AimNumber = number
+        AimTime = time
+    }
+    
+}
